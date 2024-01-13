6106a31797bceca2aaf1969e6df363d8f321c0d4
Toby Evans <tobz.nz@gmail.com>
2024-01-13 16:21:12 +1300

    update svg-icon

--- js/svg-icon.js
+++ js/svg-icon.js
@@ -1,17 +1,19 @@
 import { WebComponent } from 'WebComponent'
 
-customElements.define('svg-icon', class extends WebComponent {
+export class SvgIcon extends WebComponent {
     static props = {
         icon: '',
         width: '',
         height: '',
-        src: ''
+        src: '/images/icons.svg'
     }
 
     get template() {
         let iconPath = this.props.icon ? '#' + this.props.icon : ''
         return `<svg width="${this.props.width || 20}" height="${this.props.height || 20}">
-			<use href="${this.props.src || '/images/icons.svg'}${iconPath}"></use>
+			<use href="${this.props.src}${iconPath}"></use>
 		</svg>`
     }
-})
+}
+
+customElements.define('svg-icon', SvgIcon)
